6693d9c945a09135bc589279dea86910ea307e58
Juliana Romão de Souza <brasis957@gmail.com>
2025-10-23 21:25:37 -0300

    Atividade área do triangulo

---
+++ area_triangulo.py
@@ -0,0 +1,8 @@
+print(f"Vamos calcular a área de um triângulo\n")
+
+base= float(input("Insira o valor da base do triâgulo:"))
+altura= float(input("\nInsira o valor da altura do triângulo:"))
+
+area= (base*altura)/2
+
+print(f"\nA área do triângulo é {area}")
